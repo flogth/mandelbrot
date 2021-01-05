7e0bf9f791812e9ae701ebc5ee3d9ddd925b36ef
flothepo <f.guthmann@mailbox.org>
2021-01-05 12:04:13 +0100

    take advantage of Num instance

--- Mandelbrot.hs
+++ Mandelbrot.hs
@@ -7,9 +7,6 @@ type Complex = V2 Float
 square :: Complex -> Complex
 square (V2 a b) = V2 (a * a - b * b) (2 * a * b)
 
-add :: Complex -> Complex -> Complex
-add (V2 a b) (V2 x y) = V2 (a + x) (b + y)
-
 squaredLength :: Complex -> Float
 squaredLength (V2 a b) = a * a + b * b
 
@@ -27,7 +24,7 @@ isInMandelbrot maxIter c = go c 0
       | squaredLength zn > threshold = False
       | otherwise = go zn (i + 1)
       where
-        zn = square z `add` c
+        zn = square z + c
 
 mandelbrotSet :: V2 Int -> V2 Int -> Complex -> Int -> [V2 Int]
 mandelbrotSet (V2 width height) (V2 rStep iStep) (V2 rShift iShift) maxIter =
